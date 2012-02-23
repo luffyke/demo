784c6bb43a78a56e604e9e3ccec13edf520844ca
kxt <luffyke@gmail.com>
2012-02-24 00:13:43 +0800

    c insert sort

---
+++ c/sort/insert_sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define LENGTH 10
+
+void insert_sort(int a[], int n){
+	int i,j;
+	int temp;
+	for(i = 1; i <=n; ++i){
+		temp = a[i];
+		j = i - 1;
+		while(temp < a[j]){
+			a[j+1] = a[j--];
+			if(j < 0) break;
+		}
+		a[j+1] = temp;
+	
+	}
+}
+
+int main(){
+	int i;
+	int a[] = {2, 5, 6, 3, 7, 8, 0, 9, 12, 1};
+	insert_sort(a, LENGTH);
+	for(i = 0; i < LENGTH; ++i){
+		printf("%d", a[i]);
+		printf(",");
+	}
+}
