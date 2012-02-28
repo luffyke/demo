267d64aaf74b59fef96bbd8b9727d47c23f4e251
kxt <luffyke@gmail.com>
2012-02-28 23:41:04 +0800

    c select sort

---
+++ c/sort/select_sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define LENGTH 10
+
+void select_sort(int a[], int n){
+    int i,j,k;
+    int min,temp;
+    for(i = 0; i < n; ++i){
+        min = i;
+		temp = a[i];
+		for(j = i+1; j < n; ++j){
+			if(a[j] > a[min]){
+			min = j;
+			}
+		}
+		a[i] = a[min];
+		a[min] = temp;
+    }
+}
+
+int main(){
+   int a[] = {6,2,3,7,1,5,4,9,8,0};
+   int i;
+   select_sort(a, LENGTH);
+   for(i=0; i < LENGTH; ++i){
+       printf("%d", a[i]);
+       printf(",");
+   }
+   return 0;
+}
