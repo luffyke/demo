8d9c070b027e3841c16816985c29e327a021fad5
kxt <luffyke@gmail.com>
2013-03-01 00:19:08 +0800

    Format file and use google jquery api for linkage demo

--- js/Date/jquery.date.js
+++ js/Date/jquery.date.js
@@ -3,7 +3,7 @@
  * @author   kxt
  * @example  $("#test").date();
  */
-$.fn.date = function(year, month, day){
+$.fn.date = function (year, month, day, year_id, month_id, day_id) {
 	var _self = this;
 	
 	var begin_year = 1950;
@@ -12,87 +12,106 @@ $.fn.date = function(year, month, day){
 	var month_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	var month_max_day_array = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 	
-	 _self.html("<select id='year' name='year' style='width: 100px'></select> 年 " +
-	    		"<select id='month' name='month' style='width: 100px'></select> 月 " +
-	    		"<select id='day' name='day' style='width: 100px'></select> 日");
+	if (typeof year_id == 'undefined') {
+		year_id = 'year';
+	}
+	if (typeof month_id == 'undefined') {
+		month_id = 'month';
+	}
+	if (typeof day_id == 'undefined') {
+		day_id = 'day';
+	}
+	
+	// #id
+	var _jq_year = "#" + year_id;
+	var _jq_month = "#" + month_id;
+	var _jq_day = "#" + day_id;
+	
+	 _self.html("<select id='" + year_id + "' name='" + year_id + "' style='width: 100px'></select> 年 " +
+	    		"<select id='" + month_id + "' name='" + month_id + "' style='width: 100px'></select> 月 " +
+	    		"<select id='" + day_id + "' name='" + day_id + "' style='width: 100px'></select> 日");
 	
 	//分别获取3个下拉框
 	var year_select = _self.find("select").eq(0);
 	var month_select = _self.find("select").eq(1);
 	var day_select = _self.find("select").eq(2);
 	
-	//定义3个默认值
-	_self.data("year",["请选择", "0"]);
-	_self.data("month",["请选择", "0"]);
-	_self.data("day",["请选择", "0"]);
+	// 定义3个默认值
+	_self.data(year_id, ["请选择", "0"]);
+	_self.data(month_id, ["请选择", "0"]);
+	_self.data(day_id, ["请选择", "0"]);
 	
-	//默认下拉
-	if(_self.data("year")){
-		year_select.append("<option value='" + _self.data("year")[1] + "'>" + _self.data("year")[0] + "</option>");
+	// 默认下拉
+	if (_self.data(year_id)) {
+		year_select.append("<option value='" + _self.data(year_id)[1] + "'>" + _self.data(year_id)[0] + "</option>");
 	}
-	if(_self.data("month")){
-		month_select.append("<option value='" + _self.data("month")[1] + "'>" + _self.data("month")[0] + "</option>");
+	if (_self.data(month_id)) {
+		month_select.append("<option value='" + _self.data(month_id)[1] + "'>" + _self.data(month_id)[0] + "</option>");
 	}
-	if(_self.data("day")){
-		day_select.append("<option value='" + _self.data("day")[1] + "'>" + _self.data("day")[0] + "</option>");
+	if (_self.data(day_id)) {
+		day_select.append("<option value='" + _self.data(day_id)[1] + "'>" + _self.data(day_id)[0] + "</option>");
 	}
 	
-	//默认年份
-	for(var i = this_year; i >= begin_year; i--){
+	// 默认年份
+	for (var i = this_year; i >= begin_year; i--) {
 		year_select.append("<option value='" + i + "'>" + i + "</option>");
 	}
-	//默认月份
-	for(var i = 0; i < month_array.length; i++){
+	// 默认月份
+	for (var i = 0; i < month_array.length; i++) {
 		month_select.append("<option value='" + month_array[i] + "'>" + month_array[i] + "</option>");
 	}
-	//默认日数
-	for(var i = 0; i < 31; i++){
+	// 默认日数
+	for (var i = 0; i < 31; i++) {
 		day_select.append("<option value='" + (i+1) + "'>" + (i+1) + "</option>");
 	}
 	
 	var is_leap = false;
 	
-	//接受参数
-	if(typeof year != 'undefined'){
-		if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+	// 接受参数
+	if (typeof year != 'undefined') {
+		if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
 			is_leap = true;
 		} else {
 			is_leap = false;
 		}
-		$("#month option[index='0']").remove();
-		$("#day option[index='0']").remove(); 
+		var first_month_item = _jq_month + " option[index='0']";
+		var first_day_item = _jq_day + " option[index='0']";
+		$(first_month_item).remove();
+		$(first_day_item).remove(); 
 		//month_select[0].remove();
 		//day_select[0].remove();
 		year_select.val(year);
 		year_select.change();
     }
 	
-	year_select.change(function(){
-		month_select[0].options.length=0;
-		day_select[0].options.length=0;
+	year_select.change(function() {
+		month_select[0].options.length = 0;
+		day_select[0].options.length = 0;
 		var index = this.selectedIndex;
-		if(index == 0){	//当选择的为 "请选择" 时
-			if(_self.data("month")){
-				month_select.append("<option value='" + _self.data("month")[1] + "'>" + _self.data("month")[0] + "</option>");
+
+		if (index == 0) {
+			//当选择的为 "请选择" 时
+			if (_self.data(month_id)) {
+				month_select.append("<option value='" + _self.data(month_id)[1] + "'>" + _self.data(month_id)[0] + "</option>");
 			}
-			if(_self.data("day")){
-				day_select.append("<option value='" + _self.data("day")[1] + "'>" + _self.data("day")[0] + "</option>");
+			if (_self.data(day_id)) {
+				day_select.append("<option value='" + _self.data(day_id)[1] + "'>" + _self.data(day_id)[0] + "</option>");
 			}
-		} else{
-			var select_year_value = $('#year').val();
-			if(select_year_value % 400 == 0 || (select_year_value % 4 == 0 && select_year_value % 100 != 0)){
+		} else {
+			var select_year_value = $(_jq_year).val();
+			if (select_year_value % 400 == 0 || (select_year_value % 4 == 0 && select_year_value % 100 != 0)) {
 				is_leap = true;
 			} else {
 				is_leap = false;
 			}
-			for(var i = 0; i < month_array.length; i++){
+			for (var i = 0; i < month_array.length; i++) {
 				month_select.append("<option value='" + month_array[i] + "'>" + month_array[i] + "</option>");
 			}
-			for(var i = 0; i < 31; i++){
+			for (var i = 0; i < 31; i++) {
 				day_select.append("<option value='" + (i+1) + "'>" + (i+1) + "</option>");
 			}
-			if(typeof month != 'undefined'){
-				if(parseInt(month, 10) < 10) {
+			if (typeof month != 'undefined') {
+				if (parseInt(month, 10) < 10 && month.length == 2) {
 					month = month.substr(1, 1);
 				}
 				month_select.val(month);
@@ -101,7 +120,7 @@ $.fn.date = function(year, month, day){
 		    	month_select.val(1);
 		    	month_select.change();
 		    }
-			if(typeof day != 'undefined'){
+			if (typeof day != 'undefined') {
 				day_select.val(day);
 		    } else {
 		    	day_select.val(1);
@@ -109,21 +128,22 @@ $.fn.date = function(year, month, day){
 		}
 	}).change();
 	
-	month_select.change(function(){
-		var select_month_value = $('#month').val();
-		day_select[0].options.length=0;
-		if(select_month_value == 0){	//当选择的为 "请选择" 时
-			day_select.append("<option value='" + _self.data("day")[1] + "'>" + _self.data("day")[0] + "</option>");
+	month_select.change(function() {
+		var select_month_value = $(_jq_month).val();
+		day_select[0].options.length = 0;
+		if (select_month_value == 0) {
+			//当选择的为 "请选择" 时
+			day_select.append("<option value='" + _self.data(day_id)[1] + "'>" + _self.data(day_id)[0] + "</option>");
 		} else {
-			if(is_leap){
+			if (is_leap) {
 				month_max_day_array[1] = 29;
 			} else {
 				month_max_day_array[1] = 28;
 			}
-			for(var i = 0; i < month_max_day_array[select_month_value-1]; i++){
+			for (var i = 0; i < month_max_day_array[select_month_value-1]; i++) {
 				day_select.append("<option value='" + (i+1) + "'>" + (i+1) + "</option>");
 			}
-			if(typeof day != 'undefined'){
+			if (typeof day != 'undefined') {
 				day_select.val(day);
 			}
 		}
--- js/Linkage/jquery.provinces_city_county.js
+++ js/Linkage/jquery.provinces_city_county.js
@@ -3,98 +3,96 @@
  * @author   kxt
  * @example  $("#test").province_city_county();
  */
-$.fn.province_city_county = function(v_province,v_city,v_county){
+$.fn.province_city_county = function(v_province, v_city, v_county) {
 	var _self = this;
-	//插入3个空的下拉框
-	//_self.append("<select id='province' name='province'></select>");
-	//_self.append("<select id='city' name='city'></select>");
-	//_self.append("<select id='county' name='county'></select>");
+	// 插入3个空的下拉框
     _self.html("<select id='province' name='province' style='width: 100px'></select>" +
     		"<select id='city' name='city' style='width: 100px'></select>" +
     		"<select id='county' name='county' style='width: 100px'></select>");
-	//分别获取3个下拉框
+	// 分别获取3个下拉框
 	var sel1 = _self.find("select").eq(0);
 	var sel2 = _self.find("select").eq(1);
 	var sel3 = _self.find("select").eq(2);
 	
-	//定义3个默认值
+	// 定义3个默认值
 	_self.data("province",["请选择", ""]);
 	_self.data("city",["请选择", ""]);
 	_self.data("county",["请选择", ""]);
-	//默认省级下拉
-	if(_self.data("province")){
-		sel1.append("<option value='"+_self.data("province")[1]+"'>"+_self.data("province")[0]+"</option>");
+	// 默认省级下拉
+	if (_self.data("province")) {
+		sel1.append("<option value='" + _self.data("province")[1] + "'>" + _self.data("province")[0] + "</option>");
 	}
-	//默认城市下拉
-	if(_self.data("city")){
-		sel2.append("<option value='"+_self.data("city")[1]+"'>"+_self.data("city")[0]+"</option>");
+	// 默认城市下拉
+	if (_self.data("city")) {
+		sel2.append("<option value='" + _self.data("city")[1] + "'>" + _self.data("city")[0] + "</option>");
 	}
-	//默认县区下拉
-	if(_self.data("county")){
-		sel3.append("<option value='"+_self.data("county")[1]+"'>"+_self.data("county")[0]+"</option>");
+	// 默认县区下拉
+	if (_self.data("county")) {
+		sel3.append("<option value='" + _self.data("county")[1] + "'>" + _self.data("county")[0] + "</option>");
 	}
-	$.get('province_city.xml', function(data){
+	$.get('province_city_county.xml', function(data) {
 		var arrList = [];
-		$(data).find('province').each(function(){
+		$(data).find('province').each(function() {
 			var $province = $(this);
-			sel1.append("<option value='"+$province.attr('value')+"'>"+$province.attr('value')+"</option>");
+			sel1.append("<option value='" + $province.attr('value') + "'>" + $province.attr('value') + "</option>");
 		});
-		if(typeof v_province != 'undefined'){
+		if (typeof v_province != 'undefined') {
 			sel1.val(v_province);
 			sel1.change();
 		}
 	});
 	
-	//省级联动控制
+	// 省级联动控制
 	var index1 = "" ;
 	var provinceValue = "";
 	var cityValue = "";
-	sel1.change(function(){
-		//清空其它2个下拉框
-		sel2[0].options.length=0;
-		sel3[0].options.length=0;
+	sel1.change(function() {
+		// 清空其它2个下拉框
+		sel2[0].options.length = 0;
+		sel3[0].options.length = 0;
 		index1 = this.selectedIndex;
-		if(index1 == 0){	//当选择的为 “请选择” 时
-			if(_self.data("city")){
-				sel2.append("<option value='"+_self.data("city")[1]+"'>"+_self.data("city")[0]+"</option>");
+		// 当选择的为 "请选择" 时
+		if (index1 == 0) {
+			if (_self.data("city")) {
+				sel2.append("<option value='" + _self.data("city")[1] + "'>" + _self.data("city")[0] + "</option>");
 			}
-			if(_self.data("county")){
-				sel3.append("<option value='"+_self.data("county")[1]+"'>"+_self.data("county")[0]+"</option>");
+			if (_self.data("county")) {
+				sel3.append("<option value='" + _self.data("county")[1] + "'>" + _self.data("county")[0] + "</option>");
 			}
-		} else{
+		} else {
 			provinceValue = $('#province').val();
-			$.get('province_city.xml', function(data){
-				$(data).find("province[value='"+provinceValue+"'] > city").each(function(){
+			$.get('province_city_county.xml', function(data) {
+				$(data).find("province[value='" + provinceValue + "'] > city").each(function() {
 					var $city = $(this);
-					sel2.append("<option value='"+$city.attr('value')+"'>"+$city.attr('value')+"</option>");
+					sel2.append("<option value='" + $city.attr('value') + "'>" + $city.attr('value') + "</option>");
 				});
 				cityValue = $("#city").val();
-				$(data).find("city[value='"+cityValue+"'] > county").each(function(){
+				$(data).find("city[value='"+cityValue+"'] > county").each(function() {
 					var $county = $(this);
-					sel3.append("<option value='"+$county.attr('value')+"'>"+$county.attr('value')+"</option>");
+					sel3.append("<option value='" + $county.attr('value') + "'>" + $county.attr('value') + "</option>");
 				});
 
-                if(typeof v_city != 'undefined'){
+                if (typeof v_city != 'undefined') {
                     sel2.val(v_city);
                     sel2.change();
                 }
 
-                if(typeof v_county != 'undefined'){
+                if (typeof v_county != 'undefined') {
                     sel3.val(v_county);
                 }
 			});
 		}
 	}).change();
-	//城市联动控制
-	sel2.change(function(){
-		sel3[0].options.length=0;
+	// 城市联动控制
+	sel2.change(function() {
+		sel3[0].options.length = 0;
 		var cityValue2 = $('#city').val();
-		$.get('province_city.xml', function(data){
-			$(data).find("city[value='"+cityValue2+"'] > county").each(function(){
+		$.get('province_city_county.xml', function(data) {
+			$(data).find("city[value='"+cityValue2+"'] > county").each(function() {
 				var $county = $(this);
-				sel3.append("<option value='"+$county.attr('value')+"'>"+$county.attr('value')+"</option>");
+				sel3.append("<option value='" + $county.attr('value') + "'>" + $county.attr('value') + "</option>");
 			});
-            if(typeof v_county != 'undefined'){
+            if (typeof v_county != 'undefined') {
             	sel3.val(v_county);
             }
 		});
