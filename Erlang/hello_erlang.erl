c1fa32ad682c291c9d49444f9539f4ed8b7cad11
kxt <luffyke@gmail.com>
2013-03-02 00:42:06 +0800

    Erlang hello world

---
+++ Erlang/hello_erlang.erl
@@ -0,0 +1,23 @@
+%% Author: kxt
+-module(hello_erlang).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([print_hello_erlang/0]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+print_hello_erlang()->
+	io:format("Hello Erlang!~n").
