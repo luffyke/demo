db08c1e1ea83a085358ed2b652d5097ff20ddd87
kxt <luffyke@gmail.com>
2013-03-30 23:09:33 +0800

    Update birthdate_cal_age.php

--- php/birthdate_cal_age.php
+++ php/birthdate_cal_age.php
@@ -2,9 +2,9 @@
 	$birthdate = date_create('1988-08-15');
 	$todaydate = date_create('now');
 	if($todaydate < $birthdate){
-		$age = '你还没出生呢';
+		$age = 'Not yet born';
 	} else {
 		$interval = date_diff($todaydate, $birthdate);
-		$age = $interval->format('%y').'岁';
+		$age = $interval->format('%y').' years old';
 	}
 	echo $age;
