9dd1bd002425c1913cb5d3494865cf810b9f3859
kxt <luffyke@gmail.com>
2013-04-24 23:56:39 +0800

    Stack.java

---
+++ java/Stack.java
@@ -0,0 +1,55 @@
+package bx.basic;
+
+/**
+ * 实现一个栈，满足min() pop() push()方法的时间复杂度都为O(1).(min()返回栈中最小元素)
+ * 
+ * @author kxt
+ */
+public class Stack {
+    
+    private Item[] items;
+    private int count;
+    
+    private static final int MAX_SIZE = 100;
+    
+    class Item {
+        int data;
+        int min;
+        public Item(int data) {
+            this.data = data;
+        }
+    }
+    
+    public Stack() {
+        items = new Item[MAX_SIZE];
+    }
+    
+    public boolean push(Item item) {
+        if (count >= MAX_SIZE - 1) {
+            return false;
+        }
+        // set min value
+        item.min = count == 0 ? item.data : item.data < min() ? item.data : min();
+ 
+        // push
+        items[count++] = item;
+        return true;
+    }
+    
+    public Item pop() {
+        if (count == 0) {
+            return null;
+        }
+        // pop
+        return items[--count];
+    }
+    
+    public int min() {
+        return count != 0 ? items[count-1].min : 0; 
+    }
+    
+    public int size() {
+        return count;
+    }
+    
+}
