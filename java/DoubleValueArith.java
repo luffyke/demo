05f26e729595bddd6d5838cd9928428f0833d464
kxt <luffyke@gmail.com>
2013-07-01 15:27:21 +0800

    Update DoubleValueArith.java

--- java/DoubleValueArith.java
+++ java/DoubleValueArith.java
@@ -4,54 +4,42 @@ import java.math.BigDecimal;
 
 public class DoubleValueArith {
     
-    private static final int DEF_DIV_SCALE = 10;
+    private static final int DEFAULT_DIV_SCALE = 10;
 
     public static double add(double d1, double d2) {
-        Double double1 = new Double(d1);
-        Double double2 = new Double(d2);
-        BigDecimal db1 = new BigDecimal(double1.toString());
-        BigDecimal db2 = new BigDecimal(double2.toString());
-        return db1.add(db2).doubleValue();
-    }
-    
-    public static double subtract(double d1, double d2) {
-        Double double1 = new Double(d1);
-        Double double2 = new Double(d2);
-        BigDecimal db1 = new BigDecimal(double1.toString());
-        BigDecimal db2 = new BigDecimal(double2.toString());
-        return db1.subtract(db2).doubleValue();
-    }
-    
-    public static double multiply(double d1, double d2) {
-        Double double1 = new Double(d1);
-        Double double2 = new Double(d2);
-        BigDecimal db1 = new BigDecimal(double1.toString());
-        BigDecimal db2 = new BigDecimal(double2.toString());
-        return db1.multiply(db2).doubleValue();
-    }
-    
-    public static double divide(double d1, double d2) {
-        Double double1 = new Double(d1);
-        Double double2 = new Double(d2);
-        BigDecimal db1 = new BigDecimal(double1.toString());
-        BigDecimal db2 = new BigDecimal(double2.toString());
-        return db1.divide(db2, DEF_DIV_SCALE, BigDecimal.ROUND_HALF_UP).doubleValue();
-    }
-    
-    public static double divide(double d1, double d2, int scale) {
-        Double double1 = new Double(d1);
-        Double double2 = new Double(d2);
-        BigDecimal db1 = new BigDecimal(double1.toString());
-        BigDecimal db2 = new BigDecimal(double2.toString());
-        return db1.divide(db2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
-    }
-    
-    public static double remainder(double d1, double d2) {
-        Double double1 = new Double(d1);
-        Double double2 = new Double(d2);
-        BigDecimal db1 = new BigDecimal(double1.toString());
-        BigDecimal db2 = new BigDecimal(double2.toString());
-        return db1.remainder(db2).doubleValue();
-    }
+		BigDecimal db1 = new BigDecimal(d1);
+		BigDecimal db2 = new BigDecimal(d2);
+		return db1.add(db2).doubleValue();
+	}
+
+	public static double subtract(double d1, double d2) {
+		BigDecimal db1 = new BigDecimal(d1);
+		BigDecimal db2 = new BigDecimal(d2);
+		return db1.subtract(db2).doubleValue();
+	}
+
+	public static double multiply(double d1, double d2) {
+		BigDecimal db1 = new BigDecimal(d1);
+		BigDecimal db2 = new BigDecimal(d2);
+		return db1.multiply(db2).doubleValue();
+	}
+
+	public static double divide(double d1, double d2) {
+		BigDecimal db1 = new BigDecimal(d1);
+		BigDecimal db2 = new BigDecimal(d2);
+		return db1.divide(db2, DEFAULT_DIV_SCALE, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+
+	public static double divide(double d1, double d2, int scale) {
+		BigDecimal db1 = new BigDecimal(d1);
+		BigDecimal db2 = new BigDecimal(d2);
+		return db1.divide(db2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+
+	public static double remainder(double d1, double d2) {
+		BigDecimal db1 = new BigDecimal(d1);
+		BigDecimal db2 = new BigDecimal(d2);
+		return db1.remainder(db2).doubleValue();
+	}
 
 }
